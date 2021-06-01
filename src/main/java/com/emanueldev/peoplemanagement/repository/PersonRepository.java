c755eaeaff8b7b26699eb3088f3fe2f80bb4216b
Emanuel Vieira <emanuelvisantos99@gmail.com>
2021-05-31 22:02:34 -0300

    BugFix putting Repository annotation

--- src/main/java/com/emanueldev/peoplemanagement/repository/PersonRepository.java
+++ src/main/java/com/emanueldev/peoplemanagement/repository/PersonRepository.java
@@ -2,8 +2,10 @@ package com.emanueldev.peoplemanagement.repository;
 
 import com.emanueldev.peoplemanagement.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, UUID> {
 }
